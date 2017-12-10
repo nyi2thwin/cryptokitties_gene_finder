4c2a63a689c545fa9f17d594f3cc64c365802dfc
nyithwin <33516474+nyithwin@users.noreply.github.com>
2017-12-11 02:17:00 +0800

    finalized the script

--- profile_pulling.py
+++ profile_pulling.py
@@ -7,14 +7,16 @@ def child_gene(kitty_pair, gene=['fancy']):
     result = requests.post(end_point, json=kitty_pair)
     if result.status_code == requests.codes.ok:
         gene_result = result.json()['results']
-        check_gene = filter(lambda x: x, map(lambda x: [x[0], (('%.2f' % (x[1] * 100)))] if x[0] in gene else None, gene_result))
-        if check_gene:
-            for genes in check_gene:
-                print 'Gene %s: %s' % (genes[0], genes[1])
-                print '=' * 20
-        else:
-            print 'Hopeless couple!'
-            print '*' * 20
+        check_gene = filter(lambda x: x, map(lambda x: [x[0], (('%.2f' % (x[1] * 100)))] if x[0] in gene else None, gene_result))       
+        counter = 0
+        for genes in check_gene:
+            if counter == 0:
+                print('Sire %s: Matron %s' % (kitty_pair[0],kitty_pair[1]))
+            counter += 1
+            print('Gene %s: %s' % (genes[0], genes[1]))
+        print('=' * 20)
+        
+    
     return False
 
 def get_array_pair_by_address(ether_address):
@@ -25,7 +27,7 @@ def get_array_pair_by_address(ether_address):
         url = "https://api.cryptokitties.co/kitties?offset="+str(offset)+"&limit=20&parents=false&owner_wallet_address="+ether_address
         kitty_json = requests.get(url).json()
         for each in kitty_json['kitties']:
-            kitties_id_list.append(each['id'])
+            kitties_id_list.append(str(each['id']))
         offset += 20
         if len(kitty_json['kitties']) == 0:
             remaining = False
@@ -40,8 +42,10 @@ def get_array_pair_by_address(ether_address):
     print(len(kitties_pair_list))
     return kitties_pair_list
 
-kouples = get_array_pair_by_address("0x4ce15b37851a4448a28899062906a02e51dee267")
-kouples_babe_gene = ['fancy']
+#ether(cryptokitties) addresss here
+address = "0x4ce15b37851a4448a28899062906a02e51dee267"
+kouples = get_array_pair_by_address(address)
+kouples_babe_gene = ['fancy','jaguar']
 for kouples_genes in kouples:
     time.sleep(1)
     child_gene(kouples_genes, kouples_babe_gene)
